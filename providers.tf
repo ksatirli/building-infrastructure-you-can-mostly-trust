79e4c4df07c321cb7e55f040db1bb65e7a324df7
Kerim Satirli <k.satirli@gmail.com>
2021-11-09 00:50:41 +0100

    linting

--- providers.tf
+++ providers.tf
@@ -3,5 +3,5 @@ provider "yandex" {
   token     = var.yandex_token
   cloud_id  = var.yandex_cloud_id
   folder_id = var.yandex_folder_id
-  zone      = "ru-central1-a"
+  zone      = var.yandex_zone
 }
