919a070422a368ddd7b7d1670799de1dc13b712f
Kerim Satirli <k.satirli@gmail.com>
2021-11-09 11:29:27 +0100

    linting

--- module-example/terraform.tf
+++ module-example/terraform.tf
@@ -11,5 +11,5 @@ terraform {
 
   # define the exact version of Terraform to use
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
-  required_version = "1.0.8"
+  required_version = "1.0.10"
 }
